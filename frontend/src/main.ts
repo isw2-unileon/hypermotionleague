3789d940d21c130696d68ad340b6580f9bb3f9f4
javiestudiante <jescuj00@estudiantes.unileon.es>
2026-05-26 12:18:39 +0200

    feat(ui): HM/LEAGUE design tokens (dark+bone)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,6 +6,8 @@ import "@fontsource/bebas-neue/400.css";
 import "@fontsource/jetbrains-mono/400.css";
 import "@fontsource/jetbrains-mono/600.css";
 
+import "./design-system/tokens.css";
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
